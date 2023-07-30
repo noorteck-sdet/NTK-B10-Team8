988e2c76d07d33d39c5f9f471ee5e3de4ad4786a
noorteck-sdet <119881887+noorteck-sdet@users.noreply.github.com>
2023-07-30 12:25:50 -0400

    Update DemoDay3.java

--- src/demo/DemoDay3.java
+++ src/demo/DemoDay3.java
@@ -2,11 +2,6 @@ package demo;
 
 public class DemoDay3 {
 
-	public static void main(String[] args) {
-		System.out.println("Demo..");
-		System.out.println("Hayder...");
-		System.out.println("Harpreet");
-		System.out.println("Steven");
-		System.out.println("Maria");
-	}
+//importance of following stash process && communicating... :)
+
 }
