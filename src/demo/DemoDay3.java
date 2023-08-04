3870195468951ad2f3a30fc1a0f99811e46cc974
hayde <hayde@HayderMsi.mshome.net>
2023-08-03 20:11:07 -0400

    hi

--- src/demo/DemoDay3.java
+++ src/demo/DemoDay3.java
@@ -3,18 +3,17 @@ package demo;
 public class DemoDay3 {
 
 //importance of following stash process && communicating... :)
-	
+
 	public static void main(String[] args) {
 		System.out.println("Team 8 demo");
 		System.out.println("Harpreet....");
-		
+
 		System.out.println("Steven...");
 		System.out.println("Hayder2...");
 
-
 		System.out.println("Hi Team...");
 		System.out.println("Hi Team2...");
-
+		System.out.println("Hi Team3...");
 
 	}
 
